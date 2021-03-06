795f7f865c5075cfde3c999c68fa27766592ecef
Mani Movassagh <mani.mgh@gmail.com>
2021-03-06 01:48:32 +0100

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userNames : string='mani'
   title = 'angular';
+
+  showSome(){
+    console.log('you showed something')
+  }
 }
