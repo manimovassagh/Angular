9e777f5a3c4c9355e7850b498a6b853b1e8e539c
Mani Movassagh <mani.mgh@gmail.com>
2021-03-06 21:08:35 +0100

    checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password=''
+  includeLetters=false
   title = 'angular';
 
   onButtonClick(){
+    console.log(this.includeLetters)
 this.password='my password'  }
+
+onChangeUseLetters(){
+this.includeLetters=!this.includeLetters;
+}
 }
